d6fbe2829990809d44d18e98b5d5027fffef2304
PawanPandeyJi <pawankrpandeyjii@gmail.com>
2025-03-18 16:29:56 +0530

    create navbar and pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Elasticsearch
+        </Typography>
+        <NavLink to="/">
+          <Button sx={{color: "white", marginRight: "1rem"}}>Add Book</Button>
+        </NavLink>
+        <NavLink to="/allBooks">
+        <Button  sx={{color: "white"}}>All Books</Button>
+        </NavLink>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/AddBookForm.tsx
@@ -0,0 +1,10 @@
+
+const AddBookForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddBookForm
