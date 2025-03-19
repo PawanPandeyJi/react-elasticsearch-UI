cf68815f42bf96224040b23b06607eb80bf6e92a
PawanPandeyJi <pawankrpandeyjii@gmail.com>
2025-03-19 18:06:10 +0530

    fetch all books and render

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import './App.css'
 import AddBookForm from './pages/AddBookForm'
 import AllBooks from './pages/AllBooks'
 import Navbar from './components/Navbar'
+import { useState } from 'react'
 
 function App() {
+  const [bookCount, setBookCount] = useState(0);
+
+  const updateBookCount = (count: number) => {
+    setBookCount(count);
+  };
 
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
+      <Navbar bookCount={bookCount}/>
         <Routes>
           <Route path='/' element={<AddBookForm/>}/>
-          <Route path='/books' element={<AllBooks/>}/>
+          <Route path='/books' element={<AllBooks  updateBookCount={updateBookCount} />}/>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,17 +1,61 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 
-const AllBooks = () => {
+type RequestBooksData = {
+  id: string;
+  title: string;
+  author: string;
+  year: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type AllBooksProps = {
+  updateBookCount: (count: number) => void; 
+};
+
+const AllBooks = ({ updateBookCount }: AllBooksProps) => {
+  const [books, setBooks] = useState<RequestBooksData[]>([]);
+
+  const getAllBooks = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/books", {
+        method: "GET",
+      });
+      const res_data = await response.json();
+      if (response.ok) {
+        setBooks(res_data);
+        updateBookCount(res_data.length)
+      }
+    } catch (error) {
+      console.log("getAllBooksAPI error", error);
+    }
+  };
+
+  useEffect(() => {
+    getAllBooks();
+    <Navbar bookCount={books.length} />;
+  }, []);
   return (
-    <Card sx={{ maxWidth: 300, m: 2, boxShadow: 2 }}>
-      <CardContent>
-        <Typography variant="h6">book.title</Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          book.author
-        </Typography>
-        <Typography variant="body2">Published: book.year</Typography>
-      </CardContent>
-    </Card>
+    <>
+      <div className="booksCard">
+        {books?.map((books) => {
+          return (
+            <Card sx={{ maxWidth: 300, m: 2, boxShadow: 2 }} key={books.id}>
+              <CardContent>
+                <Typography variant="h6">{books.title}</Typography>
+                <Typography variant="subtitle1" color="text.secondary">
+                  {books.author}
+                </Typography>
+                <Typography variant="body2">Published: {books.year}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
-export default AllBooks
+export default AllBooks;
