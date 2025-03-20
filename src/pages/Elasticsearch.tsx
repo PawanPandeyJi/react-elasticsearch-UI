ffdd985b23dd3c456741bbe89d789feee96dfe41
PawanPandeyJi <pawankrpandeyjii@gmail.com>
2025-03-20 17:26:21 +0530

    Add search functionality with filters

--- src/pages/Elasticsearch.tsx
+++ src/pages/Elasticsearch.tsx
@@ -11,7 +11,8 @@ import {
   Button,
 } from "@mui/material";
 import "../App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Search } from "@mui/icons-material";
 
 type RequestBooksData = {
   took: number;
@@ -33,11 +34,30 @@ type RequestBooksData = {
 };
 
 const Elasticsearch = () => {
-  const [books, setBooks] = useState<RequestBooksData>();
+  const [books, setBooks] = useState<RequestBooksData | null>();
+  const [filter, setFilter] = useState({
+    ID: false,
+    Title: false,
+    Author: false,
+    Year: false,
+  });
+  const [allDataVisible, setAllDataVisible] = useState(false);
+  const [searchInput, setSearchInput] = useState("")
+  const [inputFocus, setInputFocus] = useState(false)
+  
+
+  const handleFilterChange = (e) => {
+    const { name, checked } = e.target;
+    setFilter((prevValue) => ({
+      ...prevValue,
+      [name]: checked,
+    }));
+  };
+  
+  const auth = btoa("elastic:zuN6vQKTpdYzr5+KI17g");
 
   const getBooks = async () => {
     try {
-      const auth = btoa("elastic:zuN6vQKTpdYzr5+KI17g");
       const response = await fetch(
         "https://localhost:9200/books_index/_search",
         {
@@ -62,8 +82,56 @@ const Elasticsearch = () => {
     } catch (error) {
       console.log("getBooks by elasticSearch error:", error);
     }
-  };
-  const allBooks = books?.hits.hits;
+};
+const allBooks = books?.hits.hits;
+
+const handleSearchSubmit = async(e) => {
+    e.preventDefault()
+    console.log(searchInput)
+    try {
+        const mustQueries = [];
+    
+        if (filter.ID) mustQueries.push({ match: { id: searchInput } });
+        if (filter.Title) mustQueries.push({ match: { title: searchInput } });
+        if (filter.Author) mustQueries.push({ match: { author: searchInput } });
+        if (filter.Year) mustQueries.push({ match: { year: searchInput } });
+    
+        const query = mustQueries.length
+          ? { bool: { should: mustQueries, minimum_should_match: 1 } }
+          : { match: { title: searchInput } };
+    
+        const response = await fetch(
+          "https://localhost:9200/books_index/_search",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Basic ${auth}`,
+            },
+            body: JSON.stringify({
+              size: 1000,
+              query: query,
+            }),
+          }
+        );
+    
+        const res_data = await response.json();
+        if (response.ok) {
+          setBooks(res_data);
+        }
+      } catch (error) {
+        console.log("handleSearchSubmit error:", error);
+      }
+  }
+
+  useEffect(() => {
+    if (Object.values(filter).some((value) => value === true) || inputFocus) {
+      setAllDataVisible(true);
+      setBooks(null);
+    } else {
+      setAllDataVisible(false);
+    }
+  }, [filter,inputFocus]);
 
   return (
     <Grid container spacing={3} sx={{ height: "93vh", padding: 3 }}>
@@ -80,18 +148,49 @@ const Elasticsearch = () => {
           <Typography variant="h6" gutterBottom>
             Search Elasticsearch
           </Typography>
-          <TextField
-            fullWidth
-            label="Search..."
-            variant="outlined"
-            sx={{ marginBottom: 2 }}
-          />
+          <form onSubmit={handleSearchSubmit} style={{display: "flex", alignItems: "center",gap: "1rem"}}>
+            <TextField
+              fullWidth
+              label="Search..."
+              variant="outlined"
+              value={searchInput}
+              sx={{ marginBottom: 2 }}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onFocus={() => setInputFocus(true)}
+              onBlur={() => setInputFocus(false)}
+            />
+            <Button type="submit" variant="outlined" color="primary" sx={{marginBottom: 2,padding: "0.95rem" }}><Search/></Button>
+          </form>
           <Typography variant="subtitle1">Filter By:</Typography>
-          <FormControlLabel control={<Checkbox />} label="Title" />
-          <FormControlLabel control={<Checkbox />} label="Author" />
-          <FormControlLabel control={<Checkbox />} label="Year" />
-          <FormControlLabel control={<Checkbox />} label="ID" />
-          <Button onClick={getBooks} variant="contained">
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Title"
+            name="Title"
+            onChange={handleFilterChange}
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Author"
+            name="Author"
+            onChange={handleFilterChange}
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Year"
+            name="Year"
+            onChange={handleFilterChange}
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="ID"
+            name="ID"
+            onChange={handleFilterChange}
+          />
+          <Button
+            onClick={getBooks}
+            variant="contained"
+            disabled={allDataVisible}
+          >
             All Datas
           </Button>
           <Typography variant="h6" mt={2}>
